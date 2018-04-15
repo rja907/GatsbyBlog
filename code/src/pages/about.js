f0f827e2500ddb3b90d5e736cb867e1d65b01a2a
Raj <kmrrajj@gmail.com>
2018-04-15 23:08:32 +0530

    added about page

---
+++ code/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About me!</h1>
+      <p>lorem ipsum</p>
+    </div>
+  );
+}
+
+module.exports = AboutPage;
