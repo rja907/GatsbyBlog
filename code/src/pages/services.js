8655ab0298c4e2a58cf9b9e7ad52b0611046712c
Raj <kmrrajj@gmail.com>
2018-04-15 23:10:12 +0530

    added services page

---
+++ code/src/pages/services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ServicesPage = () => {
+  return (
+    <div>
+      <h1>Services I offer!</h1>
+      <p>lorem ipsum</p>
+    </div>
+  );
+}
+
+module.exports = ServicesPage;
